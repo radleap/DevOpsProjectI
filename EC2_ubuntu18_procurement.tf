95d51b6b336903a6a52e10e2c34b7750a3c47cde
radleap <guy@radleap.com>
2022-01-22 15:18:28 -0500

    Adding terraform .tf script for EC2 procurement testing

--- EC2_ubuntu18_procurement.tf
+++ EC2_ubuntu18_procurement.tf
@@ -1,3 +1,16 @@
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+}
+variable "AWS_ACCESS_KEY" {
+  description = "AWS access key"
+  type        = string
+}
+variable "AWS_SECRET_KEY" {
+  description = "AWS secret key"
+  type        = string
+}
+
 terraform{
  required_providers {
   aws = {
@@ -10,14 +23,14 @@ terraform{
 
 # Configuring Provider
 provider "aws" {
- region = "us-east-1"
- access_key = "addme"
- secret_key = "addme"
+ region = var.AWS_REGION
+ access_key = var.AWS_ACCESS_KEY
+ secret_key = var.AWS_SECRET_KEY
 }
 
 # Deploying an EC2 Instance
-resource "aws_instance" "terra-instance-slave1" {
+resource "aws_instance" "terra-instance-slave2" {
  ami = "ami-0e472ba40eb589f49"
  instance_type = "t2.micro"
- tags = {Name = "terra-instance-slave1"}
+ tags = {Name = "terra-instance-slave2"}
 }
