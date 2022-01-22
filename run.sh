4dd7d361077e503efa97306cd353414561f45f64
radleap <guy@radleap.com>
2022-01-22 14:47:14 -0500

    small run.sh edit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 pwd
-sudo echo "Yahooooooooo testingggg"
+# sudo -echo "Yahooooooooo testingggg"
 # docker build -t flask-sample:latest .
 # pwd
 # docker run -d -p 5000:5000 flask-sample
